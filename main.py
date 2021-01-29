e21754be3d43c9ae8c0e3af02b9b998b400bef12
Guilherme Ramos <gui_aramos@outlook.com>
2021-01-29 20:16:40 +0900

    :sparkles: add(endpoints): add some endpoints with parameters

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
+@app.get("/")
 async def hello_world():
     return {"Hello": "World"}
+
+
+@app.get("/component/{component_id}")  # path parameter
+async def get_component(component_id: int):
+    return {"component_id": component_id}
+
+
+@app.get("/component/")  # query parameter
+async def read_component(number: int, text: str):
+    return {"number": number, "text": text}
